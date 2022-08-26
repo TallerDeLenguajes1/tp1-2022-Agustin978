3cc74075f5743021988ac67faa7b22c675153c36
loboser.sl <loboser.sl@gmail.com>
2022-08-26 19:26:50 -0300

    Terminado problema2

---
+++ Problema2/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+main();
+
+int main()
+{
+    Console.WriteLine($"El resultado de la division es: {divideNumeros()}");
+    return 0;
+}
+
+double divideNumeros()
+{
+    double num1, num2;
+
+    try
+    {
+        Console.WriteLine("Ingrese el dividendo:");
+        num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el divisor:");
+        num2 = double.Parse(Console.ReadLine());
+        return num1 / num2;
+    }catch(DivideByZeroException)
+    {
+        Console.WriteLine("No se puede dividir en 0, vuelva a promaria >:|");
+        Console.WriteLine("Maver viejo, por eso te gorrean");
+        throw;
+    }catch(FormatException)
+    {
+        Console.WriteLine("Error de formato.");
+        Console.WriteLine("Maver viejo, por eso te gorrean");
+        throw;
+    }catch(OverflowException)
+    {
+        Console.WriteLine("Error de overflow");
+        Console.WriteLine("Maver viejo, por eso te gorrean");
+        throw;
+    }catch(Exception ex)
+    {
+        Console.WriteLine($"Excepcion no conocida: {ex.Data}");
+        throw;
+    }
+}
